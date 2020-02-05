8f7422ceed5334f625bbc0ee7a0d3d1786556ec7
Adrian <murilloadrianc@gmail.com>
2020-02-05 11:10:54 -0300

    localhost

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if(process.env.NODE_ENV === "production"){
     });
 }
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 });
